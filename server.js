aa77faf665d4dd4b5e933f985ecef07e8e34df24
Your Name <graceliu@gracelius-iMac.home>
2021-12-06 16:44:55 -0500

    changed port to environment variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const{animals} = require('./data/animals.json')
-
+const PORT = process.env.PORT || 3001;
 // to instantiate the server, assign express() to the app variable so that later to chain methods to the Express.js server
 const app = express();
 // get()method require two arguments. one is a string that describes the route to fetch from. second is a callback function that will execute every time that route is accessed with a GET request.
@@ -56,6 +56,6 @@ app.get('/api/animals', (req, res) => {
 
   
 // need a method for the server to listen hence chain the listen() onto the server to do so 
-app.listen(3001,()=>{
-    console.log('API server now on port 3001')
+app.listen(PORT,()=>{
+    console.log('API server now on port ${PORT}!')
 })
