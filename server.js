01f564e7927b4b86403469b19f4e9254146caad5
Your Name <graceliu@gracelius-iMac.home>
2021-12-08 12:56:56 -0500

    post animal data

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const path = require('path')
 const express = require('express');
 const{animals} = require('./data/animals.json')
+// reserving all cap variable that are global const
 const PORT = process.env.PORT || 3001;
 // to instantiate the server, assign express() to the app variable so that later to chain methods to the Express.js server
 const app = express();
@@ -8,8 +11,11 @@ const app = express();
 //     //using send() method form res parameter(short for response ) to send "hello" to
 //     res.send("hello")
 // })
-
-// create seperate function to handle the filter (instead of inside the call back function). this function will take in the req.query as an argument and return a new filtered array
+// parse incoming string or array data
+app.use(express.urlencoded({extended:true}))
+// parse incoming JSON data
+app.use(express.json())
+// create separate function to handle the filter (instead of inside the call back function). this function will take in the req.query as an argument and return a new filtered array
 function filterByQuery(query, animalsArray){
   let personalityTraitsArray = [];
   
@@ -50,6 +56,32 @@ function findById(id, animalsArray){
   return result;
 }
 
+function createNewAnimal(body, animalsArray){
+  //console.log(body)
+  //our function's main code will go here!
+  const animal =body;
+  animalsArray.push(animal)
+  fs.writeFileSync(
+    path.join(__dirname,'./data/animals.json'),
+    JSON.stringify({animals:animalsArray}, null,2)
+  );
+  //return finished code to post route for responses
+  return animals;
+}
+
+function validateAnimals(animal){
+  if(!animal.name || typeof animal.name !== 'string' ){
+    return false;
+  }
+  if(!animal.species || typeof animal.species !== 'string'){
+    return false;
+  }
+  if(!animal.diet || typeof animal.diet !== 'string'){
+    return false;
+  }
+  return true;!
+}
+
 app.get('/api/animals', (req, res) => {
    let results =animals;
    if(req.query){
@@ -60,12 +92,26 @@ app.get('/api/animals', (req, res) => {
 // a parameter route must come after the other GET route
 app.get('/api/animals/:id', (req, res) => {
   const result  = findById(req.params.id,animals);
-  if(resizeBy){
+  if(result){
    res.json(result)
   } else {
     res.send(404);
   }
   });
+
+app.post('/api/animals',(req,res)=>{
+  //req.body is where our incoming content will be
+  //set it based on what the next index of the array will be
+  req.body.id = animals.length.toString()
+// if any data in req.body is incorrect, send 400 error back
+if(!validateAnimals(req.body)){
+  res.status(400).send('The animal is not properly formatted');
+} else {
+
+  //add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals)
+  res.json(req.body)}
+})
   
 // need a method for the server to listen hence chain the listen() onto the server to do so 
 app.listen(PORT,()=>{
